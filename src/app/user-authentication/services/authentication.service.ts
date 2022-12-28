83928b37939a72026a30859269da474501b0c1ea
Alessandro Averna <neelith@hotmail.it>
2022-12-28 18:05:21 +0100

    token validation logic and minor refactorings

--- src/app/user-authentication/services/authentication.service.ts
+++ src/app/user-authentication/services/authentication.service.ts
@@ -1,39 +1,42 @@
-import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { HttpClientService } from 'src/app/shared/services/http-client.service';
-import { environment } from 'src/environments/environment';
-import { googleOauthConfig } from '../oauth-config';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthenticationService{
-
-
+export class AuthenticationService {
   public loggedIn: boolean = false;
-  private backendApiUrl = environment.backendApiUrl;
-  public googleClientId : string = "";
-  public googleLoginUrl : string = "";
 
-  constructor(private httpClient : HttpClientService, private cookieService : CookieService) {
-    this.googleClientId = googleOauthConfig.clientId;
-    this.googleLoginUrl = googleOauthConfig.loginUrl;
-  }
+  constructor(
+    private httpClient: HttpClientService,
+    private cookieService: CookieService
+  ) {}
 
-  isLoggedIn() : boolean {
-    //TODO: check della validtà e dell'expiration date
-    let accessToken = this.cookieService.get("Finealth-Auth");
-    return accessToken && accessToken.length != 0? true : false;
+  isLoggedIn(): boolean {
+    let accessToken = this.cookieService.get('Finealth-Auth');
+    let accessTokenExist: boolean =
+      accessToken && accessToken.length != 0 ? true : false;
+    let accessTokenValid: boolean = false;
+
+    this.httpClient.isAccessTokenValid(accessToken).subscribe({
+      next: (response) => {
+        accessTokenValid = response.accessTokenValid;
+      },
+      error: () => {
+        console.error('Something went wrong during token validation.');
+      }
+    });
+
+    return accessTokenExist && accessTokenValid;
   }
 
   public signOutExternal = () => {
-    this.cookieService.delete("Finealth-Auth");
-  }
+    this.cookieService.delete('Finealth-Auth');
+  };
 
-  loginWithGoogle(credentials: string) : Observable<any>{
-    const header = new HttpHeaders().set('Content-type', 'application/json').set('withCredentials', 'true');
-    return this.httpClient.loginWithGooglePost(this.backendApiUrl + "/Session/google-callback", credentials, header);
+  loginWithGoogle(credentials: string): Observable<any> {
+    return this.httpClient.loginWithGoogle(credentials);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,3 +2,8 @@ export const environment = {
   production : false,
   backendApiUrl: "https://localhost:7114"
 }
+
+export const googleOpenIdConfig = {
+  clientId : "316890787569-d1mob96tq6n1k25bc5cke04hl8tiid7c.apps.googleusercontent.com",
+  loginUrl : "https://localhost:7114/Session/google-callback"
+}
