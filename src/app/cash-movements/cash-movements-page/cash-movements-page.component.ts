1c1b9ac704e4c9b656a9d64af5cc08bb7e8ee941
Alessandro Averna <neelith@hotmail.it>
2023-01-02 13:55:55 +0100

    edit cash movements

--- src/app/cash-movements/cash-movements-page/cash-movements-page.component.ts
+++ src/app/cash-movements/cash-movements-page/cash-movements-page.component.ts
@@ -3,18 +3,7 @@ import { CashMovement } from 'src/app/entities/cash-movement';
 import { DialogService } from 'src/app/dialogs/services/dialog.service';
 import { Category } from 'src/app/entities/category';
 import { CashMovementRepositoryService } from 'src/app/persistance/services/cash-movement-repository.service';
-import {
-  concat,
-  concatMap,
-  EMPTY,
-  finalize,
-  last,
-  map,
-  Observable,
-  of,
-} from 'rxjs';
-import { EMPTY_SUBSCRIPTION } from 'rxjs/internal/Subscription';
-import { WithID } from 'ngx-indexed-db';
+import { concatMap, EMPTY, finalize, map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-cash-movements-page',
@@ -54,34 +43,44 @@ export class CashMovementsPageComponent implements OnInit {
   onDeleteCashMovement(cashMovementId: number) {
     this.cashMovementRepository
       .deleteCashMovementById(cashMovementId)
-      .pipe(finalize(() => this.loadCashMovementList()))
-      .subscribe();
+      .subscribe(
+        (cashMovementListFromDb) =>
+          (this.cashMovementList$ = of(cashMovementListFromDb))
+      );
   }
 
   onEditCashMovement(cashMovement: CashMovement) {
-    // this.dialogService
-    //   .showEditCashMovementDialog({
-    //     cashMovement: cashMovement,
-    //     categories: this.categories,
-    //   })
-    //   .afterClosed()
-    //   .subscribe({
-    //     next: (form) => {
-    //       if (form.valid) {
-    //         var newCashMovement = new CashMovement();
-    //         newCashMovement.description = form.value.description;
-    //         newCashMovement.amount = form.value.amount;
-    //         newCashMovement.categoryId = form.value.categoryId;
-    //         newCashMovement.date = form.value.date;
-    //         newCashMovement.cashMovementId = cashMovement.cashMovementId;
-    //         const itemToRemoveIndex = this.cashMovementList.findIndex(
-    //           (item) => item.cashMovementId === cashMovement.cashMovementId
-    //         );
-    //         this.cashMovementList.splice(itemToRemoveIndex, 1, newCashMovement);
-    //       }
-    //     },
-    //     error: (err) => console.error(err),
-    //   });
+    this.dialogService
+      .showEditCashMovementDialog({
+        cashMovement: cashMovement,
+        categories: this.categories,
+      })
+      .afterClosed()
+      .pipe(
+        map((form) => {
+          let editedCashMovement: CashMovement | null = null;
+
+          if (form.valid) {
+            editedCashMovement = new CashMovement();
+            editedCashMovement.description = form.value.description;
+            editedCashMovement.amount = form.value.amount;
+            editedCashMovement.categoryId = form.value.categoryId;
+            editedCashMovement.date = form.value.date;
+            editedCashMovement.cashMovementId = cashMovement.cashMovementId;
+          }
+          debugger;
+          return editedCashMovement;
+        }),
+        concatMap((cashMovement) => {
+          if (cashMovement !== null) {
+            return this.cashMovementRepository.edit(cashMovement);
+          }
+
+          return EMPTY;
+        })
+      )
+      .pipe(finalize(() => this.loadCashMovementList()))
+      .subscribe();
   }
 
   onAddCashMovement() {
--- src/app/persistance/services/cash-movement-repository.service.ts
+++ src/app/persistance/services/cash-movement-repository.service.ts
@@ -7,17 +7,22 @@ import { CashMovement } from 'src/app/entities/cash-movement';
   providedIn: 'root',
 })
 export class CashMovementRepositoryService {
+  readonly storeName = 'CashMovements';
   constructor(private dbService: NgxIndexedDBService) {}
 
   add(cashMovement: CashMovement) : Observable<CashMovement & WithID> {
-    return this.dbService.add('CashMovements', cashMovement);
+    return this.dbService.add<CashMovement>(this.storeName, cashMovement);
+  }
+
+  edit(cashMovement: CashMovement) {
+    return this.dbService.update<CashMovement>(this.storeName, cashMovement);
   }
 
   deleteCashMovementById(cashMovementId: number): Observable<CashMovement[]> {
-    return this.dbService.delete<CashMovement>('CashMovements', cashMovementId);
+    return this.dbService.delete<CashMovement>(this.storeName, cashMovementId);
   }
 
   getAllCashMovements(): Observable<CashMovement[]> {
-    return this.dbService.getAll<CashMovement>('CashMovements');
+    return this.dbService.getAll<CashMovement>(this.storeName);
   }
 }
