fe9e08c64e3ae54a531ec66e0d63fb1b74fabe34
Alessandro Averna <neelith@hotmail.it>
2023-01-17 16:02:46 +0100

    cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   title = 'Finealth';
-  subscription: Subscription = new Subscription();
-  showSideNav: boolean = false;
-
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    this.subscription.add(
-      this.router.events.subscribe((navigation) => {
-        if (
-          navigation instanceof NavigationEnd &&
-          this.router.url !== '/login'
-        ) {
-          this.showSideNav = true;
-        }
-
-        if (
-          navigation instanceof NavigationEnd &&
-          this.router.url === '/login'
-        ) {
-          this.showSideNav = false;
-        }
-      })
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  onHomeIconClick() {
-    this.router.navigateByUrl('/');
-  }
-
-  onMovementsIconClick() {
-    this.router.navigateByUrl('/movements');
-  }
-
-  onCategoriesIconClick() {
-    this.router.navigateByUrl('/categories');
-  }
 }
