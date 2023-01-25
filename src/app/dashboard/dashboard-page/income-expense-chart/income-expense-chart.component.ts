9f0934692697b0fe75c6e63a6f3bea845a069669
Alessandro Averna <neelith@hotmail.it>
2023-01-25 17:58:10 +0100

    income expenses graph

--- src/app/dashboard/dashboard-page/income-expense-chart/income-expense-chart.component.ts
+++ src/app/dashboard/dashboard-page/income-expense-chart/income-expense-chart.component.ts
@@ -1,88 +1,48 @@
-import { Component } from '@angular/core';
-import { EChartsOption } from 'echarts';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { CashMovement } from 'src/app/entities/cash-movement';
+import { Category } from 'src/app/entities/category';
+import { EchartOptionsService } from 'src/app/graphs/services/echart-options.service';
 
 @Component({
   selector: 'app-income-expense-chart',
   templateUrl: './income-expense-chart.component.html',
   styleUrls: ['./income-expense-chart.component.scss'],
 })
-export class IncomeExpenseChartComponent {
-  incomeExpenseChartOptions: EChartsOption = {
-    color: ['#00A393', '#D91409', '#F0D701'],
-    title: {
-      left: '50%',
-      text: 'Entrate & risparmi',
-      subtext: '',
-      textAlign: 'center',
-      textStyle: { color: '#00A393' },
-    },
-    tooltip: {
-      trigger: 'axis',
-      formatter: (params: any) => {
-        let result = `${params[0].name}<br/>`;
-        let total = 0;
-        let income = 0;
-        params.forEach((param: any) => {
-          if (param.seriesName === 'Entrate') {
-            income = param.data;
-            total += param.data;
-          } else {
-            total += param.data;
-          }
-        });
-        params.forEach((param: any) => {
-          result += `
-            <span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:${
-              param.color
-            }"></span>
-            ${param.seriesName}: ${param.data} (${
-            param.seriesName === 'Entrate'
-              ? '100'
-              : ((param.data / income) * 100).toFixed(2)
-          }%)<br/>
-          `;
-        });
-        return result;
-      },
-    },
-    legend: {
-      align: 'auto',
-      data: ['Entrate', 'Uscite', 'Risparmi'],
-      bottom: 0,
-    },
-    grid: {
-      left: '20%',
-      right: '20%',
-      bottom: '5%',
-      containLabel: true,
-    },
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        name: 'Entrate',
-        type: 'line',
-        data: [2000, 2000, 3000, 1900],
-        zlevel: 1,
-      },
-      {
-        name: 'Uscite',
-        type: 'line',
-        data: [1000, 1200, 1300, 900],
-        zlevel: 2,
-      },
-      {
-        name: 'Risparmi',
-        type: 'line',
-        data: [1000, 800, 1700, 1000],
-        zlevel: 3,
-      },
-    ],
-  };
+export class IncomeExpenseChartComponent implements OnInit, OnChanges {
+  @Input() cashMovements: CashMovement[] = [];
+  @Input() categories: Category[] = [];
+
+  inputDataValid: boolean = false;
+  incomeExpenseChartOptions: any;
+  incomeExpenseChartUpdateOptions: any;
+
+  constructor(private echartOptionsService: EchartOptionsService) {}
+
+  ngOnInit(): void {
+    this.incomeExpenseChartOptions =
+      this.echartOptionsService.getIncomeExpenseChartInitOptions();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.inputDataValid =
+      this.cashMovements &&
+      this.cashMovements.length > 0 &&
+      this.categories &&
+      this.categories.length > 0
+        ? true
+        : false;
+
+    if (this.inputDataValid) {
+      this.incomeExpenseChartUpdateOptions =
+        this.echartOptionsService.getIncomeExpenseChartOptions(
+          this.cashMovements
+        );
+    }
+  }
 }
--- src/app/graphs/services/echart-options.service.ts
+++ src/app/graphs/services/echart-options.service.ts
@@ -7,6 +7,66 @@ import { Category } from 'src/app/entities/category';
   providedIn: 'root',
 })
 export class EchartOptionsService {
+  private incomeExpenseChartOptions: EChartsOption = {
+    color: ['#00A393', '#D91409', '#F0D701'],
+    title: {
+      left: '50%',
+      text: 'Entrate & risparmi',
+      subtext: '',
+      textAlign: 'center',
+      textStyle: { color: '#00A393' },
+    },
+    tooltip: {
+      trigger: 'axis',
+      formatter: (params: any) => {
+        let result = `${params[0].name}<br/>`;
+        let total = 0;
+        let income = 0;
+        params.forEach((param: any) => {
+          if (param.seriesName === 'Entrate') {
+            income = param.data;
+            total += param.data;
+          } else {
+            total += param.data;
+          }
+        });
+        params.forEach((param: any) => {
+          result += `
+            <span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:${
+              param.color
+            }"></span>
+            ${param.seriesName}: ${param.data} (${
+            param.seriesName === 'Entrate'
+              ? '100'
+              : ((param.data / income) * 100).toFixed(2)
+          }%)<br/>
+          `;
+        });
+        return result;
+      },
+    },
+    legend: {
+      align: 'auto',
+      data: [],
+      top: 30,
+    },
+    grid: {
+      left: '20%',
+      right: '20%',
+      bottom: '5%',
+      containLabel: true,
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: [],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [],
+  };
+
   private categoryChartOptions: EChartsOption = {
     color: ['#00A393', '#008F81', '#007A6E', '#00665C'],
     title: {
@@ -64,7 +124,6 @@ export class EchartOptionsService {
     categories: Category[],
     cashMovements: CashMovement[]
   ) {
-
     let categoryGraphData = this.getCategoryGraphData(
       categories,
       cashMovements
@@ -82,7 +141,121 @@ export class EchartOptionsService {
     };
   }
 
-  private getCategoryGraphData(categories: Category[], cashMovements: CashMovement[]) {
+  getIncomeExpenseChartInitOptions() {
+    return this.incomeExpenseChartOptions;
+  }
+
+  getIncomeExpenseChartOptions(cashMovements: CashMovement[]) {
+    let incomeExpenseGraphData = this.getIncomeExpenseGraphData(cashMovements);
+
+    return {
+      series: [
+        {
+          name: 'Entrate',
+          type: 'line',
+          data: incomeExpenseGraphData.series.entrate,
+          zlevel: 1,
+        },
+        {
+          name: 'Uscite',
+          type: 'line',
+          data: incomeExpenseGraphData.series.uscite,
+          zlevel: 2,
+        },
+        {
+          name: 'Risparmi',
+          type: 'line',
+          data: incomeExpenseGraphData.series.risparmi,
+          zlevel: 3,
+        },
+      ],
+      legend: {
+        data: ['Entrate', 'Uscite', 'Risparmi'],
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: incomeExpenseGraphData.xAxis.data,
+      },
+    };
+  }
+
+  private getIncomeExpenseGraphData(cashMovements: CashMovement[]) {
+    const entrate: Record<string, number> = {};
+    const uscite: Record<string, number> = {};
+    const risparmi: Record<string, number> = {};
+    const xAxis = { data: [] as string[] };
+
+    cashMovements.forEach((cashMovement) => {
+      const date = new Date(cashMovement.date);
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      const key = `${month}-${year}`;
+
+      if (xAxis.data.indexOf(key) === -1) {
+        entrate[key] = 0;
+        uscite[key] = 0;
+        risparmi[key] = 0;
+        xAxis.data.push(`${month}-${year}`);
+      }
+
+      if (cashMovement.amount > 0) {
+        entrate[key] += cashMovement.amount;
+      } else {
+        uscite[key] += cashMovement.amount;
+      }
+
+      risparmi[key] = entrate[key] + uscite[key];
+    });
+
+    xAxis.data.sort((a, b) => {
+      const aData = a.split('-');
+      const bData = b.split('-');
+      const aMonth = parseInt(aData[0], 10);
+      const aYear = parseInt(aData[1], 10);
+      const bMonth = parseInt(bData[0], 10);
+      const bYear = parseInt(bData[1], 10);
+      const aDate = new Date(aYear, aMonth - 1);
+      const bDate = new Date(bYear, bMonth - 1);
+      return aDate.getTime() - bDate.getTime();
+    });
+
+    const sortedEntrate = this.sortRecordByDateKey(entrate);
+    const sortedUscite = this.sortRecordByDateKey(uscite);
+    const sortedRisparmi = this.sortRecordByDateKey(risparmi);
+
+    return {
+      series: {
+        entrate: Object.values(sortedEntrate),
+        uscite: Object.values(sortedUscite),
+        risparmi: Object.values(sortedRisparmi),
+      },
+      xAxis,
+    };
+  }
+
+  private sortRecordByDateKey(record: Record<string, number>) {
+    return Object.fromEntries(
+      Object.entries(record).sort((a, b) => this.sortByDate(a, b))
+    );
+  }
+
+  private sortByDate(a: any, b: any) {
+    const aData = a[0].split('-');
+    const bData = b[0].split('-');
+    const aMonth = parseInt(aData[0], 10);
+    const aYear = parseInt(aData[1], 10);
+    const bMonth = parseInt(bData[0], 10);
+    const bYear = parseInt(bData[1], 10);
+    const aDate = new Date(aYear, aMonth - 1);
+    const bDate = new Date(bYear, bMonth - 1);
+    return aDate.getTime() - bDate.getTime();
+  }
+
+  private getCategoryGraphData(
+    categories: Category[],
+    cashMovements: CashMovement[]
+  ) {
     return categories
       .map((category) => {
         let categoryCashMovements = cashMovements.filter(
